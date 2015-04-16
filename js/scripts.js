69f4c21ed26c3e539a64fd59335f806fdd927426
Reid Baldwin & Kyle Bulloch <student@epicodus.com>
2015-04-16 11:47:52 -0700

    fix basic category functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,23 @@
 var Category = {
-  tasks = []
+  tasks: []
 }
 
 var Task = {
   complete: false
 }
 
-$(document).ready(function(){
-  $("#category").submit(function() {
+$(document).ready(function() {
+  $("form#category").submit(function(event) {
+    event.preventDefault();
     var new_category = Object.create(Category);
     new_category.tag= $("input#new-category").val();
-  })
-})
+    $("#categories").append("<li>"+new_category.tag +"</li>");
+  });
+
+  $("task").submit(function() {
+    var new_task = Object.create(Task);
+    new_task.tag= $("input#new-task").val();
+  });
+
+
+});
