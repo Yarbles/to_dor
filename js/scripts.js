6fba9a6d54636fdbd8a2d5a97a2473db70d4e7c1
Reid Baldwin & Kyle Bulloch <student@epicodus.com>
2015-04-16 12:00:28 -0700

    fix task add functionality

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,17 @@ var Task = {
 }
 
 $(document).ready(function() {
-  $("form#category").submit(function(event) {
+  $("#category").submit(function(event) {
     event.preventDefault();
     var new_category = Object.create(Category);
     new_category.tag= $("input#new-category").val();
     $("#categories").append("<li>"+new_category.tag +"</li>");
   });
-
-  $("task").submit(function() {
+  $("#add-new-task").submit(function(event) {
+    event.preventDefault();
     var new_task = Object.create(Task);
     new_task.tag= $("input#new-task").val();
+    $("#tasks").append("<li>"+new_task.tag +"</li>");
   });
 
 
