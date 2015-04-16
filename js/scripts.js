16e00c5a1ef92121d500e7ee56ef4f259a5238f9
Reid Baldwin & Kyle Bulloch <student@epicodus.com>
2015-04-16 16:47:25 -0700

    prints too many tasks and everything sucks

--- js/scripts.js
+++ js/scripts.js
@@ -7,18 +7,43 @@ var Task = {
 }
 
 $(document).ready(function() {
-  $("#category").submit(function(event) {
+
+  $("#add-new-category").submit(function(event) {
     event.preventDefault();
     var new_category = Object.create(Category);
     new_category.tag= $("input#new-category").val();
-    $("#categories").append("<li>"+new_category.tag +"</li>");
+    $("#categories").append("<li><span class='category'>"+new_category.tag+"</li>");
+
+    $(".category").click(function() {
+      //show the add task form
+      $("#add-task").hide();
+
+      $("#add-task").show();
+      $("#tasks").text("");
+      $(".category").off();
+
+      $("#category-name").text(new_category.tag);
+
+
+    });
+      $("#add-task").submit(function(event) {
+        var new_task = Object.create(Task);
+        event.preventDefault();
+        new_task.tag= $("input#new-task").val();
+        new_category.tasks.push(new_task);
+
+        $("#tasks").empty();
+
+        new_category.tasks.forEach(function(task) {
+          $("#tasks").append("<li>" + task.tag + "</li>");
+        });
+
+        // $("#tasks").append("<li>"+new_task.tag +"</li>");
+      });
+
   });
-  $("#add-new-task").submit(function(event) {
-    event.preventDefault();
-    var new_task = Object.create(Task);
-    new_task.tag= $("input#new-task").val();
-    $("#tasks").append("<li>"+new_task.tag +"</li>");
-  });
+
+
 
 
 });
